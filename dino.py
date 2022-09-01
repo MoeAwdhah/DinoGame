767d5e50e24c5c991a06b177e0ab0fe1f882d5e1
MoeAwdhah <106633183+MoeAwdhah@users.noreply.github.com>
2022-09-01 06:59:41 +0300

    Update dino.py

--- dino.py
+++ dino.py
@@ -9,20 +9,21 @@ from pynput.keyboard import Key, Controller
 button = Controller()
 
 while keyboard.is_pressed('q') == False:
+# to turn the program off press q
 
     e,f,v = pyautogui.pixel(1220, 318)
 
     if e == 83:
     
         while keyboard.is_pressed('q') == False:
-            r,g,b = pyautogui.pixel(770, 419)
+            b = pyautogui.pixel(770, 419)
 
             if b == 83:
                 button.press(Key.up)
                 time.sleep(0.1)
                 button.release(Key.up)
                 print("far")
-
+                # the program prints far when it's looking for a grey pixels far from the player character (for testing perposes)
             else:
                 break
 
@@ -34,3 +35,4 @@ while keyboard.is_pressed('q') == False:
             time.sleep(0.2)
             button.release(Key.up)
             print("close")
+            # the program prints close when it's looking for a grey pixels close to the player character (for testing perposes)
